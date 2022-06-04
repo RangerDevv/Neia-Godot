72cf8e37df91cd68543aaf15264aa261311a0b31
Rangerdevv <meerabdul2008@gmail.com>
2022-06-04 14:12:32 -0400

    Bug fixes and main menu

--- Scripts/ToolButton.gd
+++ Scripts/ToolButton.gd
@@ -5,15 +5,6 @@ extends ToolButton
 # var b = "text"
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
-
+func _on_PLAY_pressed():
+	get_tree().change_scene_to(load('res://Main.tscn'))
 	pass # Replace with function body.
