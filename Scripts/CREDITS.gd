7ba5782e9d4ccbecf6f0eb740d0b9f66232f9838
Rangerdevv <meerabdul2008@gmail.com>
2022-06-07 19:03:15 -0400

    images

---
+++ Scripts/CREDITS.gd
@@ -0,0 +1,12 @@
+extends ToolButton
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+
+func _on_CREDITS_pressed():
+	get_tree().change_scene_to(load('res://Credits.tscn'))
+	pass # Replace with function body.
