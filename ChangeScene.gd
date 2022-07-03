005b1a42822c151b9848846d73790b7b73fe26ec
Rangerdevv <meerabdul2008@gmail.com>
2022-07-03 11:56:59 -0400

    added collisions

---
+++ ChangeScene.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Area2D_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	get_tree().change_scene_to(load('res://Village.tscn'))
+	pass # Replace with function body.
